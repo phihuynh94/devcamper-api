cfd424b33cbf260ca70c3f22cf4f6421c6de08ed
Phi Huynh <phi.huynh@modivcare.com>
2023-12-28 22:30:28 -0700

    add Course CRUD operations & Aggregate calc avg course cost

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -28,7 +28,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   );
 
   // Finding resource
-  query = Bootcamp.find(JSON.parse(queryStr));
+  query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
 
   // Select fields
   if (select) {
@@ -47,7 +47,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
   // Pagination
   const page = parseInt(req.query.page, 10) || 1;
-  const limit = parseInt(req.query.limit, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 100;
   const startIndex = (page - 1) * limit;
   const endIndex = page * limit;
   const total = await Bootcamp.countDocuments();
@@ -131,13 +131,15 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route   POST /api/v1/bootcamps/:id
 // @access  Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     );
   }
+
+  await bootcamp.deleteOne();
 
   res.status(200).json({ success: true, data: bootcamp });
 });
